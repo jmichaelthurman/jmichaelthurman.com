14ded787d57d77d7cd4a67a36ee64ca3673eff5d
J. Michael Thurman <jmt@jmichaelthurman.com>
2018-09-21 06:34:49 -0500

    Update index.js
    changed to on hiatus

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,21 +9,20 @@ const IndexPage = (props) => {
   return (
     <div className={styles.indexContent}>
       <div className={styles.hero}>
-        <h1>Hi.</h1>
+        <h1>Cheers!.</h1>
         <p>
-          I&apos;m Michael, a technologist with five years of full-time IT operations experience, and twenty years of part-time consultancy on web and technology solutions. 
-          I&apos;m now focused (trying to focus) on full stack systems hosted on major cloud providers.
+          This blog is on hiatus. 
         </p>
       </div>
-      {postEdges.map(post => (
-        <Article
-          title={post.node.frontmatter.title}
-          date={post.node.frontmatter.date}
-          excerpt={post.node.excerpt}
-          slug={post.node.fields.slug}
-          key={post.node.fields.slug}
-        />
-      ))}
+//       {postEdges.map(post => (
+//         <Article
+//           title={post.node.frontmatter.title}
+//           date={post.node.frontmatter.date}
+//           excerpt={post.node.excerpt}
+//           slug={post.node.fields.slug}
+//           key={post.node.fields.slug}
+//         />
+//       ))}
     </div>
   );
 };
